d37c048f25853dc32c7f6ac5603c31395202162f
knotuteye <kevinotuteye@gmail.com>
2020-04-17 23:02:55 +0000

    debugged setup.js

--- database_cred.js
+++ database_cred.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//     host: 'host-name',
+//     user: 'username',
+//     password: 'password',
+//     database: 'database-name',
+//     table: 'table-name'
+// }
+
 module.exports = {
-    host: 'host-name',
-    user: 'username',
-    password: 'password',
-    database: 'database-name',
-    table: 'table-name'
+    host: 'localhost',
+    user: 'root',
+    password: 'kevinrulz',
+    database: 'linkzz',
+    table: 'tatata'
 }
--- setup.js
+++ setup.js
@@ -6,22 +6,21 @@ const db_cred = require('./database_cred')
  */
 function createDB() {
     let connection = mysql.createConnection({
+        multipleStatements: true,
         host: db_cred.host,
         user: db_cred.user,
         password: db_cred.password
     });
     connection.connect()
-    connection.query(`CREATE DATABASE ${db_cred.database};
-    USE ${db_cred.database};
-    CREATE TABLE ${db_cred.table}; (
-        short varchar(6) NOT NULL,
-        link varchar(255) NOT NULL,
-        createdAt date NOT NULL,
-        PRIMARY KEY (short),
-    );
-    `,
+    connection.query(`CREATE DATABASE ${db_cred.database};USE ${db_cred.database};CREATE TABLE ${db_cred.table} (short varchar(6) NOT NULL, link varchar(255) NOT NULL,createdAt date NOT NULL,PRIMARY KEY (short));`,
         function (error, results, fields) {
-            if (error) throw error;
+            if (error)
+                throw error
+            else
+                console.log('No errors. Installed Successfully');
         })
     connection.end();
+
 }
+
+createDB();
