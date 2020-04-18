0e64f3df6adbe7436cf7a65b6b7dd559ac83e869
knotuteye <kevinotuteye@gmail.com>
2020-04-18 14:17:26 +0000

    patched db_cred

--- database_cred.js
+++ database_cred.js
@@ -1,15 +1,7 @@
-// module.exports = {
-//     host: 'host-name',
-//     user: 'username',
-//     password: 'password',
-//     database: 'database-name',
-//     table: 'table-name'
-// }
-
 module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: 'kevinrulz',
-    database: 'linkzz',
-    table: 'tatata'
+    host: 'host-name',
+    user: 'username',
+    password: 'password',
+    database: 'database-name',
+    table: 'table-name'
 }
