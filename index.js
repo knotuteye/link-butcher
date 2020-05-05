16b98f203e8d6c263691779aa9ff3f9dbe04a741
knotuteye <kevinotuteye@gmail.com>
2020-05-05 12:24:21 +0000

    added env.PORT

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000
 
 const fetchLink = require('./src/getLink')
 const fetchShort = require('./src/getShort')
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+	res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/new', (req, res) => {
-    fetchShort(req.query.link, (short) => {
-        res.send(short)
-    })
+	fetchShort(req.query.link, (short) => {
+		res.send(short)
+	})
 })
 
 app.get('/:short', (req, res) => {
-    fetchLink(req.params.short, (link) => {
-        link == null ? res.send('Link not found or has expired') : res.redirect(`http://${link}`)
-    })
+	fetchLink(req.params.short, (link) => {
+		link == null
+			? res.send('Link not found or has expired')
+			: res.redirect(`http://${link}`)
+	})
 })
 
 app.listen(port, console.log(`Listening on port ${port}`))
